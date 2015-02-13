9220b6958f756b1036dc160777792c125265d9c8
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2015-02-13 23:20:09 +0200

    Changed: Re-factored code for improved standards.

--- UrlShort/src/com/urlshort/UpdateServlet.java
+++ UrlShort/src/com/urlshort/UpdateServlet.java
@@ -1,14 +1,13 @@
 package com.urlshort;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 /**
  * Servlet implementation class UpdateServlet
@@ -17,72 +16,70 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/shorten")
 public class UpdateServlet extends HttpServlet {
-	private static String database = UrlShortStrings.EMPTY;
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    private static String database = UrlShortStrings.EMPTY;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public UpdateServlet() {
-		super();
-	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/plain");
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
 
-		// PrintWriter for writing to the response
-		final PrintWriter print = response.getWriter();
+        // PrintWriter for writing to the response
+        final PrintWriter print = response.getWriter();
 
-		// Retrieve the url parameter e.g. POST /shorten?url=http://www.example.com
-		final String url = request.getParameter("url");
+        // Retrieve the url parameter e.g. POST /shorten?url=http://www.example.com
+        final String url = request.getParameter("url");
 
-		// Create a new data access object.
-		final UrlDAO urlDAO = new UrlDAO(database);
+        // Create a new data access object.
+        final UrlDAO urlDAO = new UrlDAO(database);
 
-		// Determine whether successful or not.
-		boolean isSuccess = false;
-		if (url == null || url.isEmpty()) {
-			print.println(UrlShortStrings.UPDATE_NULL_OR_EMPY);
-		} else if (!UrlShortHelper.isValidUrl(url)) {
-			print.println(UrlShortStrings.UPDATE_INVALID_FORMAT);
-		} else {
-			final UrlShort urlShort = urlDAO.insert(url);
-			if (urlShort == null) {
-				print.println(UrlShortStrings.UPDATE_NOT_INSERTED);
-			} else {
-				isSuccess = true;
-				print.println(UrlShortStrings.SUCCESS_ONLY + urlShort.getUrl());
-			}
-		}
+        // Determine whether successful or not.
+        boolean isSuccess = false;
+        if (url == null || url.isEmpty()) {
+            print.println(UrlShortStrings.UPDATE_NULL_OR_EMPY);
+        } else if (!UrlShortHelper.isValidUrl(url)) {
+            print.println(UrlShortStrings.UPDATE_INVALID_FORMAT);
+        } else {
+            final UrlShort urlShort = urlDAO.insert(url);
+            if (urlShort == null) {
+                print.println(UrlShortStrings.UPDATE_NOT_INSERTED);
+            } else {
+                isSuccess = true;
+                print.println(UrlShortStrings.SUCCESS_ONLY + urlShort.getUrl());
+            }
+        }
 
-		if (urlDAO != null) {
-			print.println();
-			final ArrayList<UrlShort> list = urlDAO.getUrlShorts();
-			if (list.isEmpty()) {
-				print.println(UrlShortStrings.DATABASE_EMPTY);
-			} else {
-				print.println(UrlShortStrings.DEMO);
-				for (final UrlShort us : list) {
-					print.println(us);
-					print.println();
-				}
-			}
-		}
+        print.println();
+        final ArrayList<UrlShort> list = urlDAO.getUrlShorts();
+        if (list.isEmpty()) {
+            print.println(UrlShortStrings.DATABASE_EMPTY);
+        } else {
+            print.println(UrlShortStrings.DEMO);
+            for (final UrlShort us : list) {
+                print.println(us);
+                print.println();
+            }
+        }
 
-		// Close the printing object.
-		print.close();
+        // Close the printing object.
+        print.close();
 
-		// Set the appropriate status code.
-		response.setStatus(isSuccess ? HttpServletResponse.SC_OK : HttpServletResponse.SC_NOT_FOUND);
-	}
+        // Set the appropriate status code.
+        response.setStatus(isSuccess ? HttpServletResponse.SC_OK : HttpServletResponse.SC_NOT_FOUND);
+    }
 
-	@Override
-	public void init() throws ServletException {
-		super.init();
-		database = getServletContext().getRealPath(UrlShortStrings.DATABASE_PATH);
-	}
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        database = getServletContext().getRealPath(UrlShortStrings.DATABASE_PATH);
+    }
 }
--- UrlShort/src/com/urlshort/UrlShortStrings.java
+++ UrlShort/src/com/urlshort/UrlShortStrings.java
@@ -1,20 +1,20 @@
 package com.urlshort;
 
-public final class UrlShortStrings {
-	public static final String DATABASE_EMPTY = "Error: The database is empty";
-	public static final String DATABASE_PATH = "/WEB-INF/data/urlshort.db";
-	public static final String DELETE_ERROR = "Error: Deleting database table";
-	public static final String DELETE_SUCCESS = "Success: Deleted database table";
-	public static final String DEMO = "[DEMO] Datebase entries: ";
-	public static final String EMPTY = "";
-	public static final String EXPAND_INVALID_FORMAT = "Error: Short id is an invalid format";
-	public static final String EXPAND_NOT_FOUND = "Error: Not found in the database";
-	public static final String EXPAND_NULL_OR_EMPTY = "Error: Short id is null or empty";
-	public static final String REDIRECT_INVALID_FORMAT = "Error: Short id is an invalid format";
-	public static final String REDIRECT_NOT_FOUND = EXPAND_NOT_FOUND;
-	public static final String REDIRECT_NULL_OR_EMPTY = "Error: Short id is null or empty";
-	public static final String SUCCESS_ONLY = "Success: ";
-	public static final String UPDATE_INVALID_FORMAT = "Error: Url is an invalid format";
-	public static final String UPDATE_NOT_INSERTED = "Error: Not inserted into the database";
-	public static final String UPDATE_NULL_OR_EMPY = "Error: Url is null or empty";
+final class UrlShortStrings {
+    public static final String DATABASE_EMPTY = "Error: The database is empty";
+    public static final String DATABASE_PATH = "/WEB-INF/data/urlshort.db";
+    public static final String DELETE_ERROR = "Error: Deleting database table";
+    public static final String DELETE_SUCCESS = "Success: Deleted database table";
+    public static final String DEMO = "[DEMO] Datebase entries: ";
+    public static final String EMPTY = "";
+    public static final String EXPAND_INVALID_FORMAT = "Error: Short id is an invalid format";
+    public static final String EXPAND_NOT_FOUND = "Error: Not found in the database";
+    public static final String EXPAND_NULL_OR_EMPTY = "Error: Short id is null or empty";
+    public static final String REDIRECT_INVALID_FORMAT = "Error: Short id is an invalid format";
+    public static final String REDIRECT_NOT_FOUND = EXPAND_NOT_FOUND;
+    public static final String REDIRECT_NULL_OR_EMPTY = "Error: Short id is null or empty";
+    public static final String SUCCESS_ONLY = "Success: ";
+    public static final String UPDATE_INVALID_FORMAT = "Error: Url is an invalid format";
+    public static final String UPDATE_NOT_INSERTED = "Error: Not inserted into the database";
+    public static final String UPDATE_NULL_OR_EMPY = "Error: Url is null or empty";
 }
