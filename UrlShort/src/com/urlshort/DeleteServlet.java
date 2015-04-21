0a3237767287593c3ead5e7e1062baa29f5903e8
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2015-04-21 22:45:22 +0300

    Added json output for when clearing the database

--- UrlShort/src/com/urlshort/DeleteServlet.java
+++ UrlShort/src/com/urlshort/DeleteServlet.java
@@ -1,6 +1,7 @@
 package com.urlshort;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -8,6 +9,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
 
 /**
  * Servlet implementation class DeleteServlet
@@ -29,15 +32,22 @@ public class DeleteServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set the response to write JSON
+		response.setContentType(UrlShortStrings.WRITE_JSON);
+		
 		// Create a new data access object.
 		final UrlDAO urlDAO = new UrlDAO(database);
-		if (urlDAO.clear()) {
-			System.out.println(UrlShortStrings.DELETE_SUCCESS);
-		} else {
-			System.out.println(UrlShortStrings.DELETE_ERROR);
-		}
+
+		// Determine whether successful or not.
+		final boolean isSuccess = urlDAO.clear();
+		final String message = isSuccess ? UrlShortStrings.DELETE_SUCCESS : UrlShortStrings.DELETE_ERROR;
+
+		final JSONObject json = new JSONObject();
+		json.put(UrlShortStrings.JSON_KEY_MESSAGE, isSuccess ? null : message);
+		json.put(UrlShortStrings.JSON_KEY_SUCCESS, isSuccess);
 
 		final ArrayList<UrlShort> list = urlDAO.getUrlShorts();
 		if (!list.isEmpty()) {
@@ -46,6 +56,21 @@ public class DeleteServlet extends HttpServlet {
 				System.out.println();
 			}
 		}
+
+		// PrintWriter for writing to the response
+		final PrintWriter print = response.getWriter();
+
+		// Print the json
+		print.println(json);
+		
+		// Debugging only
+		System.out.println(json);
+
+		// Close the printing object.
+		print.close();
+
+		// Set the appropriate status code.
+		response.setStatus(isSuccess ? HttpServletResponse.SC_OK : HttpServletResponse.SC_NOT_FOUND);
 	}
 
 	@Override
